c74538b639304f3221961e8dab5c30ba50792601
Javier Elohim Burgos Chaguezac <84932679+JavierBurgos-web@users.noreply.github.com>
2023-01-03 18:51:04 -0500

    archivo3

---
+++ Day_3.py
@@ -0,0 +1,4 @@
+from IPython.display import YouTubeVideo
+# importamos la libreria de YouTube para mostrar un video embedido en la ventana.
+YouTubeVideo('jej8qlzlAGw')
+#Notesé que solo se debe registrar el codigo del video, no la url completa.
